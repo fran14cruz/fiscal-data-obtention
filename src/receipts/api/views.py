776e97cdb5feb74f8e665123dd468891c6fd9e91
Francisco Cruz <fran14.ich@gmail.com>
2019-07-19 14:33:54 +0300

    gitignore update

--- src/receipts/api/views.py
+++ src/receipts/api/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics
 from receipts.models import Receipt
 from .serializers import ReceiptSerializer
 
-from rest_framework import viewsets
 from django.http import JsonResponse
 
 class ReceiptPostAPIView(generics.CreateAPIView):
@@ -31,18 +30,3 @@ class ReceiptRudView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Receipt.objects.all()
-
-class ReceiptViewSet(viewsets.ModelViewSet):
-    
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-
-        status = True
-        response_data = {
-            "status": str(status),
-            "receipt": serializer.data
-        }
-        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
